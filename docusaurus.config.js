abb70f10cf284c7bb60f1bf3d1cf6ce88ca889cb
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-12 15:44:54 +0200

    github config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://lisp-docs.github.io/cl-language-reference',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'lisp-docs', // Usually your GitHub org/user name.
+  projectName: 'cl-language-reference', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: "main",
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
