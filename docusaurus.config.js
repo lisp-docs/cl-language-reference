ea45a7a0c471100ad9727ce4dcc2d22e3c8da9c9
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-26 14:10:31 +0200

    updating github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/lisp-docs/cl-language-reference/',
             label: 'GitHub',
             position: 'right',
           },
