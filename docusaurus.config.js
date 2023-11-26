ff9d6a4ec429e048325af37e2f85eeb4f7e2afcb
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-26 15:15:52 +0200

    updating edit links for github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lisp-docs/cl-language-reference/tree/develop/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lisp-docs/cl-language-reference/tree/develop/blog/',
         },
         theme: {
           customCss: './src/css/custom.css',
