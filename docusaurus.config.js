71903fe2a6ffdbfa297da8842bc6d6e597ec32be
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-26 14:07:47 +0200

    updating project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} LISP Docs, MIT License. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
