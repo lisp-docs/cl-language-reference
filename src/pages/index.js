dbe1251903ba98cf04835fda9e3e20904f628147
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-30 13:09:04 +0200

    updating link in start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/contribute">
             How to Contribute Tutorial - 5min ⏱️
           </Link>
         </div>
