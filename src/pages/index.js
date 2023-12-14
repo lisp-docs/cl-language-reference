e2d84a5a9d15e1208cf889739b5b449d9b40596c
Daniel Nussenbaum <daninus14@gmail.com>
2023-12-14 13:00:29 +0200

    fixing another link breaking deployment...

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/contribute">
+            to="/docs/contribute">
             How to Contribute Tutorial - 5min ⏱️
           </Link>
         </div>
